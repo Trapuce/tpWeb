5165b642d229dba6004d4cd5012609e13bc6f36c
Daouda traore <dt2040760@gmail.com>
2023-09-25 22:09:34 +0200

    Ajout de l la fonction draw

--- interaction.js
+++ interaction.js
@@ -1,53 +1,59 @@
-
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-  this.initX , this.initY , this.finalX , this.finalY = 0 
-  this.mouseHeld = false;
+    this.initX = 0;
+    this.initY = 0;
+    this.finalX = 0;
+    this.finalY = 0;
+    this.isDrawing = false;
 
-  
-  this.mouseClick = function (evt) {
-    var res  = getMousePosition(canvas,evt)
-    this.initX = res.x 
-    this.initY = res.y
-    this.mouseHeld = true;
-    console.log(res.x)
-    console.log(res.y)
-  }.bind(this)
+    this.mouseClick = function(evt) {
+        var res = getMousePosition(canvas, evt);
+        this.initX = res.x;
+        this.initY = res.y;
+        this.isDrawing = true;
+    }.bind(this);
 
-  this.mouseMovement = function(evt) {
-    if(this.mouseHeld) {
-      var res  = getMousePosition(canvas,evt)
-      this.finalX = res.x 
-      this.finalY = res.y
-      console.log(res.x)
-      console.log(res.y)
-    }
-  }.bind(this)
+    // Met à jour les coordonnées finales pendant le mouvement de la souris
+    this.mouseMovement = function(evt) {
+        if (this.isDrawing) {
+            var res = getMousePosition(canvas, evt);
+            this.finalX = res.x;
+            this.finalY = res.y;
+            this.draw(); // Appelle la fonction de dessin à chaque mouvement
+        }
+    }.bind(this);
 
-  this.mouseRelease= function(evt) {
-    if(this.mouseHeld) {
-      var res  = getMousePosition(canvas,evt)
-      console.log(res.x)
-      console.log(res.y)
-      this.mouseHeld = false;
-    }
-  }.bind(this)
-  canvas.addEventListener('mousedown', this.mouseClick,false)
-  canvas.addEventListener('mousemove', this.mouseMovement,false)
-  canvas.addEventListener('mouseup', this.mouseRelease,false)
-};
+    this.mouseRelease = function(evt) {
+        if (this.isDrawing) {
+            var res = getMousePosition(canvas, evt);
+            this.finalX = res.x;
+            this.finalY = res.y;
+            this.draw();
+            this.isDrawing = false;
+        }
+    }.bind(this);
 
 
+    this.draw = function() {
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.moveTo(this.initX, this.initY);
+        ctx.lineTo(this.finalX, this.finalY);
+        ctx.stroke();
+    }.bind(this);
 
-// Place le point de l'événement evt relativement à la position du canvas.
+    canvas.addEventListener('mousedown', this.mouseClick, false);
+    canvas.addEventListener('mousemove', this.mouseMovement, false);
+    canvas.addEventListener('mouseup', this.mouseRelease, false);
+}
+
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-};
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
 
-
-
+var canvas = document.getElementById('myCanvas');
+var dnd = new DnD(canvas);
