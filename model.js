e505eb58f3dcbbe1b4007398f3deeae759e5e94b
Ethan Cadorel <ethan.cadorel@hotmail.fr>
2023-09-26 09:34:14 +0200

    update heritage canvas view

--- model.js
+++ model.js
@@ -1,23 +1,27 @@
-function Shape(color, thickness) {
+function Shape(thickness, color) {
     this.color = color;
     this.thickness = thickness;
 }
 
-function Rectangle(x, y, width, height, color, thickness) {
-    Shape.call(this,color,thickness);
+function Rectangle(x, y, width, height, thickness, color) {
+    Shape.call(this, color, thickness);
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
 }
+Rectangle.prototype = new Shape();
 
-function Line(x, y, x2, y2) {
-    Shape.call(this,color,thickness);
+
+function Line(x, y, x2, y2, thickness, color) {
+    Shape.call(this, color, thickness);
     this.X = x;
     this.y = y;
     this.x2 = x2;
     this.y2 = y2;
 }
+Line.prototype = new Shape();
+
 
 function Drawing(tab) {
     this.tab = tab;
